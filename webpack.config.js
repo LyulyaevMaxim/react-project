b191c12b9ad25a5a10ca2fa29c6eb74f532fa501
Lyulyaev Maxim <lyulyaevma@lad24.ru>
2018-01-10 17:15:49 +0300

    Очистка рабочей директории при инициализации и копирование html

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,15 @@
 const path = require('path');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
 
 module.exports = {
-	entry: './src/index.js',
+	entry: {
+		app: './src/index.js',
+		print: './src/print.js'
+	},
+	devtool: 'inline-source-map',
+	plugins: [new CleanWebpackPlugin(['dist'])],
 	output: {
-		filename: 'bundle.js',
+		filename: '[name].bundle.js',
 		path: path.resolve(__dirname, 'dist')
 	},
 	module: {
