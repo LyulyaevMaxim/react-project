5ed6268047b4ca456f89c17dbf045acc205babd6
Lyulyaev Maxim <lyulyaevma@lad24.ru>
2018-01-10 16:26:17 +0300

    Support for CSS, Images and Fonts

---
+++ src/index.js
@@ -0,0 +1,21 @@
+import _ from 'lodash';
+import './style.css';
+import Icon from './icon.png';
+
+function component() {
+	var element = document.createElement('div');
+
+	// Lodash, now imported by this script
+	element.innerHTML = _.join(['<p>Hello', 'webpack</p>'], ' ');
+	element.classList.add('hello');
+
+	// Add the image to our existing div.
+	var myIcon = new Image();
+	myIcon.src = Icon;
+
+	element.appendChild(myIcon);
+
+	return element;
+}
+
+document.body.appendChild(component());
