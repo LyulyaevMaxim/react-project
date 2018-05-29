b224e2e1fdc1e56b723d03bf6e24f9c0672296ea
Lyulyaev Maxim <lyulyaevma@lad24.ru>
2018-05-29 11:46:39 +0300

    + updated main packages + non TS custom modules -> JS + update navigation

--- src/js/components/header/index.js
+++ src/js/components/header/index.js
@@ -3,33 +3,25 @@ import { NavLink } from 'react-router-dom'
 import { hot } from 'react-hot-loader'
 import styles from './index.scss'
 
-function Header({ path, components }) {
-	const links = [
-		{ title: 'Главная', href: `${path}` },
-		{ title: 'Таблица', href: `${path}table` },
-		{ title: 'Форма', href: `${path}form` },
-		{ title: 'Lazy Load', href: `${path}lazy` }
-		// { title: 'Переключатели', href: `${path}switches` }
-	]
-
-	return (
-		<header className={styles["header"]}>
-			<nav className={styles["header-nav"]}>
-				{links.map(({ title, href }, index) => (
-					<NavLink
-						to={href}
-                        className={styles["header-nav-link"]}
-						activeClassName={styles['active']}
-						// onMouseOver={components[index].load}
-						exact
-						key={title}
-					>
-						{title}
-					</NavLink>
-				))}
-			</nav>
-		</header>
-	)
+function Header({ routes }) {
+  return (
+    <header className={styles['header']}>
+      <nav className={styles['header-nav']}>
+        {routes.map(({ title, path, component }) => (
+          <NavLink
+            to={path}
+            className={styles['header-nav-link']}
+            activeClassName={styles['active']}
+            onMouseOver={component.load}
+            exact
+            key={title}
+          >
+            {title}
+          </NavLink>
+        ))}
+      </nav>
+    </header>
+  )
 }
 
 export default hot(module)(Header)
