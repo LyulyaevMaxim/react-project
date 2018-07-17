009a0c1fbd122fbad820b2153814f7a92ac25bcd
Lulaev Maxim <maksim.lulaev@revelsystems.com>
2018-07-17 15:13:18 +0300

    Added utils/decorators

---
+++ src/js/utils/decorators.js
@@ -0,0 +1,48 @@
+//функция fn выполнится лишь 1 раз
+export function once(fn) {
+  let returnValue
+  let canRun = true
+  return function runOnce() {
+    if (canRun) {
+      returnValue = fn.apply(this, arguments)
+      canRun = false
+    }
+    return returnValue
+  }
+}
+
+//функция fn вызовется лишь после "count"-вызовов
+export function after(count, fn) {
+  let runCount = 0
+  return function runAfter() {
+    runCount = runCount + 1
+    if (runCount >= count) {
+      return fn.apply(this, arguments)
+    }
+  }
+}
+
+//функция fn будет вызываться не чаще раза в interval
+export function throttle(fn, interval) {
+  let lastTime
+  return function throttled() {
+    let timeSinceLastExecution = Date.now() - lastTime
+    if (!lastTime || timeSinceLastExecution >= interval) {
+      fn.apply(this, arguments)
+      lastTime = Date.now()
+    }
+  }
+}
+
+//функция fn будет выполняться с задержкой wait миллисекунд
+function debounce(fn, wait) {
+  let timer
+  return function debounced() {
+    clearTimeout(timer)
+    let args = arguments
+    let that = this
+    timer = setTimeout(function callOriginalFn() {
+      fn.apply(that, args)
+    }, wait)
+  }
+}
