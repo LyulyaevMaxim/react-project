71903a147b70d45f4e9d8e7fbe9a5746a6647830
Lulaev Maxim <maksim.lulaev@revelsystems.com>
2018-07-17 11:11:39 +0300

    Add 'browserlist' in root package.json. Optimized the size of the bundle by reducing the list of supported browsers.
    http://browserl.ist/?q=%3E%3D+1%25

--- webpack/configs/babelrc.js
+++ webpack/configs/babelrc.js
@@ -7,7 +7,7 @@ const presets = [
     '@babel/preset-env',
     {
       targets: {
-        browsers: ['> 1%', 'last 2 versions'],
+        browsers: require(`${root}/package.json`).browserslist,
       },
       modules: false,
       loose: true,
