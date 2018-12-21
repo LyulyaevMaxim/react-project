851c207137ca0593c6f5f1ed20bfed33662880a8
Lyulyaev Maxim <maxim.lulaev@gmail.com>
2018-12-21 05:43:03 +0300

    stylelint

--- .stylelintrc.js
+++ .stylelintrc.js
@@ -1,9 +1,11 @@
 const path = require('path'),
-  root = path.resolve(__dirname, './')
+  root = path.resolve(__dirname, './'),
+  isLintFix = false //TODO: add activation in package.json
 
 module.exports = {
   extends: ['stylelint-config-recommended-scss', 'stylelint-config-airbnb', 'stylelint-config-standard', 'stylelint-config-prettier'],
   plugins: [
+    'stylelint-order',
     'stylelint-declaration-block-no-ignored-properties',
     'stylelint-no-indistinguishable-colors', //need will check
     'stylelint-high-performance-animation',
@@ -11,11 +13,11 @@ module.exports = {
     'stylelint-color-format',
     'stylelint-declaration-use-variable',
     'stylelint-group-selectors', // only easy cases :(
-    'stylelint-order',
+    'stylelint-prettier' //TODO: it has conflict with stylelint-order, need run last or use webpack
   ],
   rules: {
     'plugin/declaration-block-no-ignored-properties': true,
-    "plugin/stylelint-no-indistinguishable-colors": true,
+    'plugin/stylelint-no-indistinguishable-colors': true,
     'plugin/no-low-performance-animation-properties': [
       true,
       { ignoreProperties: ['color', 'background-color', 'border-color'] },
@@ -34,8 +36,10 @@ module.exports = {
       { ignoreValues: ['inherit', 'transparent', 'currentColor'] },
     ]],
     'plugin/stylelint-group-selectors': true,
-    // 'order/order': ['custom-properties', 'dollar-variables', 'declarations', 'at-rules', 'rules'],
+    // 'order/order': [/*'custom-properties', 'dollar-variables', 'declarations', 'at-rules', 'rules'*/, { 'disableFix': !isLintFix }],
     'order/properties-alphabetical-order': true,
+    "prettier/prettier": isLintFix,
+
     'selector-type-no-unknown': [true, { ignore: ['custom-elements'] }],
     'block-no-empty': null,
     'at-rule-no-unknown': null,
