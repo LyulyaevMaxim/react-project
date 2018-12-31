54490561fdf379aa1a02f8c1ae784f32e064d15a
Lyulyaev Maxim <maxim.lulaev@gmail.com>
2018-12-31 06:00:24 +0300

    Modernizr and polyfills

--- src/js/index.jsx
+++ src/js/index.jsx
@@ -1,27 +1,32 @@
-import '~modules/polyfills'
+import polyfills from '~modules/polyfills'
 import '~css/vendors.css'
 import React from 'react'
 import ReactDOM from 'react-dom'
 import { Provider } from 'react-redux'
 import { ConnectedRouter } from 'connected-react-router'
-import { store, history } from '~store'
-import App from './components/app'
-import { isDev } from '~constants'
 
-!isDev &&
-  'serviceWorker' in navigator &&
-  window.addEventListener('load', () => {
-    navigator.serviceWorker
-      .register(`${window.location.origin}/assets/js/sw.js`)
-      .then(registration => console.log('SW registered: ', registration))
-      .catch(registrationError => console.log('SW registration failed: ', registrationError))
+Promise.all(polyfills)
+  .then(() => {
+    const { store, history } = require('~store'),
+      App = require('./components/app').default,
+      { isDev } = require('~constants')
+
+    !isDev &&
+      'serviceWorker' in navigator &&
+      window.addEventListener('load', () => {
+        navigator.serviceWorker
+          .register(`${window.location.origin}/assets/js/sw.js`)
+          .then(registration => console.log('SW registered: ', registration))
+          .catch(registrationError => console.log('SW registration failed: ', registrationError))
+      })
+
+    ReactDOM.render(
+      <Provider {...{ store }}>
+        <ConnectedRouter {...{ history }}>
+          <App />
+        </ConnectedRouter>
+      </Provider>,
+      document.getElementById('root')
+    )
   })
-
-ReactDOM.render(
-  <Provider {...{ store }}>
-    <ConnectedRouter {...{ history }}>
-      <App />
-    </ConnectedRouter>
-  </Provider>,
-  document.getElementById('root')
-)
+  .catch(error => console.error('Failed fetching polyfills', error))
--- src/js/modules/polyfills/index.js
+++ src/js/modules/polyfills/index.js
@@ -1,8 +1,21 @@
 /* Modernizr is the industry standard for feature detection tests — MDN */
 import Modernizr from 'modernizr'
 
-console.log(Modernizr)
-// import 'intersection-observer'
-// <dialog /> => react-a11y-dialog || GoogleChrome/dialog-polyfill
-// <details /> => details-element-polyfill
-// position: fixed  => stickyfilljs
+export default [
+  !window.IntersectionObserver &&
+    import('intersection-observer' /* webpackChunkName: "polyfill->intersection-observer" */),
+
+  !Modernizr.testProp('position', 'sticky ') &&
+    import('stickyfilljs' /* webpackChunkName: "polyfill->position-sticky" */),
+
+  !Modernizr.details && import('details-element-polyfill' /* webpackChunkName: "polyfill->detailsElem" */),
+
+  typeof HTMLDialogElement !== 'function' &&
+    import('dialog-polyfill' /*or react-a11y-dialog */ /* webpackChunkName: "polyfill->dialogElem" */).then(module => {
+      window.dialogPolyfill = module.default
+    }),
+
+  !Modernizr.fetch && import('whatwg-fetch' /* webpackChunkName: "polyfill->fetch" */),
+].filter(Boolean)
+
+// if (Modernizr.hasEvent('blur'))
